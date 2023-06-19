4c6af05712820593859d794470a6bd7f01fd1771
= <ayazumman@gmail.com>
2023-06-19 15:19:33 +0400

    ADD

---
+++ java.js
@@ -0,0 +1,66 @@
+
+
+let qiymet=8;
+
+
+
+
+
+function myfunction1(){
+    let y=document.querySelectorAll(".button")
+for(let i=0;i<y.length;i++){
+    y[i].style.backgroundColor="hsl(216, 11%, 28%)"
+}
+let x=document.getElementById("button1")
+x.style.backgroundColor="hsl(217, 12%, 63%)"
+  
+ 
+
+}
+function myfunction2(){
+    let y=document.querySelectorAll(".button")
+for(let i=0;i<y.length;i++){
+    y[i].style.backgroundColor="hsl(216, 11%, 28%)"
+}
+
+let x=document.getElementById("button2")
+x.style.backgroundColor="hsl(217, 12%, 63%)"
+
+}
+function myfunction3(){
+    let y=document.querySelectorAll(".button")
+for(let i=0;i<y.length;i++){
+    y[i].style.backgroundColor="hsl(216, 11%, 28%)"
+}
+let x=document.getElementById("button3")
+x.style.backgroundColor="hsl(217, 12%, 63%)"
+
+}
+function myfunction4(){
+    let y=document.querySelectorAll(".button")
+for(let i=0;i<y.length;i++){
+    y[i].style.backgroundColor="hsl(216, 11%, 28%)"
+}
+let x=document.getElementById("button4")
+x.style.backgroundColor="hsl(217, 12%, 63%)"
+ 
+}
+function myfunction5(){
+    let y=document.querySelectorAll(".button")
+for(let i=0;i<y.length;i++){
+    y[i].style.backgroundColor="hsl(216, 11%, 28%)"
+}
+let x=document.getElementById("button5")
+x.style.backgroundColor="hsl(217, 12%, 63%)"
+
+
+}
+
+function mysub(){
+
+qiymet=4;
+document.getElementById("hh").innerHTML="dfgdf";
+
+} 
+console.log(qiymet)
+
